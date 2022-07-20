2886ab44a305a2d173489d10520c7261ff294894
HoangTien339 <tiennh21@fpt.edu.vn>
2022-07-20 07:28:40 +0700

    Connect JDBC

---
+++ src/lesson7_demo_jdbc/DemoJdbcConnect.java
@@ -0,0 +1,27 @@
+package lesson7_demo_jdbc;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DemoJdbcConnect {
+    public static void main(String[] args) {
+        try {
+            // B1: Load Driver
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            // B2: Kết nối
+            String dbUser = "sa", dbPass = "Aa@123456",
+                dbUrl = "jdbc:sqlserver://localhost:1433;"
+                    + ";databaseName=su22b2_sof203";
+            
+            Connection conn = DriverManager
+                .getConnection(dbUrl, dbUser, dbPass);
+            System.out.println("Kết nối thành công");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
